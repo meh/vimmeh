1c8a24ce9c21aa65c95250aa6da6cbe53faa9471
meh <meh@schizofreni.co>
2015-10-31 16:43:24 +0100

    setup: add setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-mkdir -p ~/vim/bundle
+mkdir -p ~/.vim/bundle
 ln -s ~/.vim/vimrc ~/.vimrc
 mkdir -p ~/.vim/tmp/{backup,swap,undo}
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
